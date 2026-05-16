7e349f3c5d5e3fd53b7498738e02d3a2cf8680e7
Suryakanta Samal <suryakantasamal774@gmail.com>
2026-05-17 03:50:04 +0530

    update

--- flyza-frontend/src/components/Navbar.js
+++ flyza-frontend/src/components/Navbar.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import flyzaLogo from "../assets/flyza-logo.png"; // ✅ Your logo file
 import "./Navbar.css";
 
 function Navbar() {
   return (
     <header className="navbar">
+      {/* Left: Logo */}
       <div className="nav-left">
-        <Link to="/" title="Home">🏠</Link>
-        <Link to="/categories" title="Categories">☰</Link>
+        <Link to="/" className="logo-link">
+          <img src={flyzaLogo} alt="Flyza Logo" className="nav-logo" />
+        </Link>
       </div>
 
+      {/* Center: Search bar */}
       <div className="nav-center">
         <input
           type="text"
@@ -19,11 +23,13 @@ function Navbar() {
         <button className="search-btn">Search</button>
       </div>
 
+      {/* Right: Navigation links */}
       <div className="nav-right">
-        <Link to="/cart" title="Cart">🛒</Link>
-        <Link to="/favorites" title="Favorites">❤️</Link>
-        <Link to="/about" title="About">ℹ️</Link>
-        <Link to="/rewards" title="Reward Points">⭐</Link>
+        <Link to="/categories" title="Categories">Categories</Link>
+        <Link to="/cart" title="Cart">Cart</Link>
+        <Link to="/favorites" title="Favorites">Favorites</Link>
+        <Link to="/about" title="About">About</Link>
+        <Link to="/rewards" title="Reward Points">Rewards</Link>
       </div>
     </header>
   );
